b6940279079e86aef8b768cef3cb3a4d4af93abd
glove.tux <alexzsk@outlook.com>
2024-09-04 21:29:48 -0400

    Create colorscheme.nix

---
+++ colorscheme.nix
@@ -0,0 +1,23 @@
+colors = {
+background = "#000000"; # or it can be #1c1c1c
+foreground = "#ffffff";
+regular0 = "#000000";
+regular1 = "#333333";
+regular2 = "#666666";
+regular3 = "#999999";
+regular4 = "#cccccc";
+regular5 = "#999999";
+regular6 = "#666666";
+regular7 = "#333333";
+bright0 = "#000000";
+bright1 = "#333333";
+bright2 = "#666666";
+bright3 = "#999999";
+bright4 = "#cccccc";
+bright5 = "#999999";
+bright6 = "#666666";
+bright7 = "#ffffff";
+};
+};
+};
+}
